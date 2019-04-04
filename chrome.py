72f7e6260b2922e67e1372d458681b714de8da38
Jack Stanley <leechauisjackmasfather@aliyun.com>
2019-04-04 18:23:10 +0800

    Create chrome.py

---
+++ chrome.py
@@ -0,0 +1,10 @@
+# Chrome arguments see: https://peter.sh/experiments/chromium-command-line-switches/
+from selenium import webdriver
+
+url = "https://www.nytimes.com"
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_argument('--proxy-server=socks://127.0.0.1:1080')
+browser = webdriver.Chrome(options=chrome_options)
+browser.get(url)
+browser.get_screenshot_as_file('screen.png')
+browser.close()
