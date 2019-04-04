48f38e5ca29d132dd7bb81a228bcacb74ddd4298
Jack Stanley <leechauisjackmasfather@aliyun.com>
2019-04-04 18:28:22 +0800

    Update chrome.py

--- chrome.py
+++ chrome.py
@@ -4,6 +4,8 @@ from selenium import webdriver
 url = "https://www.nytimes.com"
 chrome_options = webdriver.ChromeOptions()
 chrome_options.add_argument('--proxy-server=socks://127.0.0.1:1080')
+chrome_options.add_argument('--window-size=1920,3840')
+chrome_options.add_argument('headless')
 browser = webdriver.Chrome(options=chrome_options)
 browser.get(url)
 browser.get_screenshot_as_file('screen.png')
